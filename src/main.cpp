22539f987573e90d1d5bf8dd0de491845765678f
April White <april@opsnlops.io>
2022-03-07 20:16:43 -0800

    Four spaces, not two

--- src/main.cpp
+++ src/main.cpp
@@ -28,46 +28,48 @@ static MQTT mqtt = MQTT(String(CREATURE_NAME));
 void setup()
 {
 
-  // Fire up the logging framework first
-  l.init();
+    // Fire up the logging framework first
+    l.init();
+    l.info("Hello! I am up and running on a %s", ARDUINO_VARIANT);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  NetworkConnection network = NetworkConnection();
-  network.connectToWiFi();
+    NetworkConnection network = NetworkConnection();
+    l.info("Hello! I am up and running on a %s", ARDUINO_VARIANT);
+    network.connectToWiFi();
 
-  // Register ourselves in mDNS
-  CreatureMDNS creatureMDNS = CreatureMDNS(CREATURE_NAME, CREATURE_POWER);
-  creatureMDNS.registerService(666);
-  creatureMDNS.addStandardTags();
+    // Register ourselves in mDNS
+    CreatureMDNS creatureMDNS = CreatureMDNS(CREATURE_NAME, CREATURE_POWER);
+    creatureMDNS.registerService(666);
+    creatureMDNS.addStandardTags();
 
-  // Set the time
-  Time time = Time();
-  time.init();
-  time.obtainTime();
+    // Set the time
+    Time time = Time();
+    time.init();
+    time.obtainTime();
 
-  // Get the location of the magic broker
-  MagicBroker magicBroker;
-  magicBroker.find();
+    // Get the location of the magic broker
+    MagicBroker magicBroker;
+    magicBroker.find();
 
-  // Connect to MQTT
-  l.info("Starting MQTT");
-  mqtt.connect(magicBroker.ipAddress, magicBroker.port);
-  mqtt.subscribe(String("cmd"), 0);
+    // Connect to MQTT
+    l.info("Starting MQTT");
+    mqtt.connect(magicBroker.ipAddress, magicBroker.port);
+    mqtt.subscribe(String("cmd"), 0);
 
-  // Enable OTA
-  // setup_ota(String(CREATURE_NAME));
-  // start_ota();
+    // Enable OTA
+    // setup_ota(String(CREATURE_NAME));
+    // start_ota();
 
-  // Tell MQTT we're alive
-  mqtt.publish(String("status"), String("I'm alive!!"), 0, false);
-  mqtt.startHeartbeat();
+    // Tell MQTT we're alive
+    mqtt.publish(String("status"), String("I'm alive!!"), 0, false);
+    mqtt.startHeartbeat();
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(250);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(250);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
 }
